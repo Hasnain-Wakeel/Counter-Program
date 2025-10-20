16e105a381eb09f6bfa6f536feb48a84e25b4e74
Hasnain-Wakeel <wakeelanjum112@gmail.com>
2025-10-20 09:12:25 +0500

    An Interactive Counter Program Created !!!

---
+++ script.js
@@ -0,0 +1,21 @@
+// Counter Program Functionality :
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const counter = document.getElementById("counter");
+
+let count = 0;
+
+decreaseBtn.onclick = function (){
+    count--;
+    counter.textContent = count;
+}
+resetBtn.onclick = function (){
+    count = 0;
+    counter.textContent = count;
+}
+increaseBtn.onclick = function (){
+    count++;
+    counter.textContent = count;
+}

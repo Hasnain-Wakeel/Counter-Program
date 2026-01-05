bcf2f82355bfa590c3d37c70be0e3dd69ebd2574
Hasnain-Wakeel <wakeelanjum112@gmail.com>
2026-01-05 11:45:10 +0500

    Making Counter Program's Second Approach

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// ---------------------------- First Approach ---------------------------
+
+
 // Counter Program Functionality :
 
 const decreaseBtn = document.getElementById("decreaseBtn");
@@ -9,13 +12,48 @@ let count = 0;
 
 decreaseBtn.onclick = function (){
     count--;
-    counter.textContent = count;
+    // counter.textContent = count;
+    // counter.innerText = count;
+    counter.innerHTML = count;
+    console.log(count);
 }
 resetBtn.onclick = function (){
     count = 0;
     counter.textContent = count;
+    console.log(count);
 }
 increaseBtn.onclick = function (){
     count++;
     counter.textContent = count;
+    console.log(count);
 }
+
+
+// ---------------------------- Second Approach ---------------------------
+
+
+// Counter Program Functionality :
+
+// const decreaseBtn = document.getElementById("decreaseBtn");
+// const increaseBtn = document.getElementById("increaseBtn");
+// const resetBtn = document.getElementById("resetBtn");
+// const counter = document.getElementById("counter");
+
+// let count = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
